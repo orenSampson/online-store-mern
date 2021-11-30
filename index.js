b69d4302913d3de2b01a70f75dd82f055da63e33
oren sampson <oren.sampson@gmail.com>
2021-11-30 23:10:45 +0200

    attempt to fix

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const path = require("path");
 
 const initProductsSeeder = require("./seeders/productsSeeder");
 const initCategoriesSeeder = require("./seeders/categoriesSeeder");
@@ -46,6 +47,10 @@ app.use("/api/auth", authRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 // app start configuration
