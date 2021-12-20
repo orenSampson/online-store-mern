414620adeabd2a7a498e6e8ba35b8eb2f18b09d0
oren sampson <oren.sampson@gmail.com>
2021-12-20 20:30:53 +0200

    Product Modal component

--- client/src/components/products/ProductModal/ProductModal.js
+++ client/src/components/products/ProductModal/ProductModal.js
@@ -1,36 +1,61 @@
 import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
 
-import PriceFormatter from "../../general/PriceFormater/PriceFormatter";
+import PriceFormatter from "../../general/PriceFormatter/PriceFormatter";
 import ShowAmount from "../../general/ShowAmount/ShowAmount";
 import styles from "./ProductModal.module.scss";
 
 const ProductModal = (props) => {
-  const cssClasses = [
-    styles["ProductModal"],
-    props.show ? styles["ProductModalOpen"] : styles["ProductModalClosed"],
-  ];
+  const cssClasses = props.show
+    ? styles["ProductModalOpen"]
+    : styles["ProductModalClosed"];
 
   return (
-    <div className={cssClasses.join(" ")}>
-      <img
-        className={styles["ProductModal-picture"]}
-        src={props.image}
-        alt="product"
+    <Card
+      sx={{
+        position: "fixed",
+        zIndex: "200",
+        top: "30%",
+        left: "25%",
+        width: "50%",
+      }}
+      className={cssClasses}
+    >
+      <CardMedia
+        sx={{ height: "14rem" }}
+        component="img"
+        image={props.image}
+        alt="image"
       />
-      <div>
-        <p>{props.title}</p>
-        <p>{props.description}</p>
-        <p>{props.category}</p>
-        <p>
+      <CardContent>
+        <Typography variant="h6" gutterBottom>
+          {props.title}
+        </Typography>
+
+        <Typography variant="body1" gutterBottom>
+          {props.description}
+        </Typography>
+
+        <Typography variant="body1" gutterBottom>
+          {props.category}
+        </Typography>
+
+        <Typography variant="h6" gutterBottom>
           <ShowAmount
             amount={props.amount}
             showAddToCartBtn={props.showAddToCartBtn}
             isCart={props.isCart}
           />
-        </p>
-        <PriceFormatter price={props.price} />
-      </div>
-    </div>
+        </Typography>
+
+        <Typography variant="h6">
+          <PriceFormatter price={props.price} />
+        </Typography>
+      </CardContent>
+    </Card>
   );
 };
 
