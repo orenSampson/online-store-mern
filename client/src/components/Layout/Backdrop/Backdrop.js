615399e7aaef21d1bb4caacf751f2d15660c3af1
oren sampson <oren.sampson@gmail.com>
2022-02-12 17:13:15 +0200

    Backdrop: refactor + custom style from props

--- client/src/components/Layout/Backdrop/Backdrop.js
+++ client/src/components/Layout/Backdrop/Backdrop.js
@@ -1,7 +1,19 @@
-import styles from "./Backdrop.module.scss";
+import { Box } from "@mui/material";
+
+const styles = {
+  Backdrop: {
+    position: "fixed",
+    zIndex: "100",
+    top: "0",
+    left: "0",
+    width: "100vw",
+    height: "100vh",
+    backgroundColor: "rgba(0, 0, 0, 0.8)",
+  },
+};
 
 const Backdrop = (props) => {
-  return props.showBackDrop && <div className={styles["Backdrop"]}></div>;
+  return props.showBackDrop && <Box sx={styles.Backdrop}></Box>;
 };
 
 export default Backdrop;
