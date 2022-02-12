a0cade056fad14baa450cc22188f66529d3e9c8e
oren sampson <oren.sampson@gmail.com>
2022-02-12 17:04:26 +0200

    ShowAmount: refactor + custom style from props

--- client/src/components/general/ShowAmount/ShowAmount.js
+++ client/src/components/general/ShowAmount/ShowAmount.js
@@ -1,13 +1,22 @@
-import React, { Fragment } from "react";
+import { Box } from "@mui/material";
+
+const styles = {
+  ShowAmount: {},
+};
 
 const ShowAmount = (props) => {
+  styles.ShowAmount = {
+    ...styles.ShowAmount,
+    ...(props.customStyle || {}),
+  };
+
   const Amount = props.isCart
     ? `${props.amount} added to cart`
     : props.showAddToCartBtn
     ? `${props.amount} left in stock`
     : `${props.amount} purchased`;
 
-  return <Fragment>{Amount}</Fragment>;
+  return <Box sx={styles.ShowAmount}>{Amount}</Box>;
 };
 
 export default ShowAmount;
