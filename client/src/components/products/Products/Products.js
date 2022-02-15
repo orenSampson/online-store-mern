48fffec109ed87939b4009abda7b7aaacb38db02
oren sampson <oren.sampson@gmail.com>
2022-02-15 10:33:52 +0200

    Products: refactor + custom style from props

--- client/src/components/products/Products/Products.js
+++ client/src/components/products/Products/Products.js
@@ -1,5 +1,14 @@
-import styles from "./Products.module.scss";
+import { Box } from "@mui/material";
 import Product from "../Product/Product";
+
+const styles = {
+  Products: {
+    width: "100vw",
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-evenly",
+  },
+};
 
 const Products = (props) => {
   const RenderProducts = props.products?.map((product) => (
@@ -17,7 +26,12 @@ const Products = (props) => {
     />
   ));
 
-  return <div className={styles["Products"]}>{RenderProducts}</div>;
+  styles.Products = {
+    ...styles.Products,
+    ...(props.customStyle || {}),
+  };
+
+  return <Box sx={styles.Products}>{RenderProducts}</Box>;
 };
 
 export default Products;
