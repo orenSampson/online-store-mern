8cf7015afeff7026f14c6d0a27ff623c9b9e51ed
oren sampson <oren.sampson@gmail.com>
2022-02-26 19:21:55 +0200

    auth naming refactoring

--- client/src/store/auth/actions.js
+++ client/src/store/auth/actions.js
@@ -1,89 +1,89 @@
 import * as types from "./types";
 import { AUTH_INITIAL_STATE } from "./reducers";
 
-export const authIsLoggedinSetter = (payload) => ({
+export const isLoggedinSetter = (payload) => ({
   type: types.AUTH_ISLOGGEDIN_SETTER,
   payload,
 });
 
-export const authLoggedUserEmailSetter = (payload) => ({
+export const loggedUserEmailSetter = (payload) => ({
   type: types.AUTH_LOGGEDUSEREMAIL_SETTER,
   payload,
 });
 
-export const authEmailSetter = (payload) => ({
+export const emailSetter = (payload) => ({
   type: types.AUTH_EMAIL_SETTER,
   payload,
 });
 
-export const authEmailErrorSetter = (payload) => ({
+export const emailErrorSetter = (payload) => ({
   type: types.AUTH_EMAIL_ERROR_SETTER,
   payload,
 });
 
-export const authPasswordSetter = (payload) => ({
+export const passwordSetter = (payload) => ({
   type: types.AUTH_PASSWORD_SETTER,
   payload,
 });
 
-export const authPasswordErrorSetter = (payload) => ({
+export const passwordErrorSetter = (payload) => ({
   type: types.AUTH_PASSWORD_ERROR_SETTER,
   payload,
 });
 
-export const authIsLoggedinReset = () => ({
+export const isLoggedinReset = () => ({
   type: types.AUTH_ISLOGGEDIN_SETTER,
   payload: AUTH_INITIAL_STATE.isLoggedin,
 });
 
-export const authLoggedUserEmailReset = () => ({
+export const loggedUserEmailReset = () => ({
   type: types.AUTH_LOGGEDUSEREMAIL_SETTER,
   payload: AUTH_INITIAL_STATE.loggedUserEmail,
 });
 
-export const authEmailReset = () => ({
+export const emailReset = () => ({
   type: types.AUTH_EMAIL_SETTER,
   payload: AUTH_INITIAL_STATE.email,
 });
 
-export const authEmailErrorReset = () => ({
+export const emailErrorReset = () => ({
   type: types.AUTH_EMAIL_ERROR_SETTER,
   payload: AUTH_INITIAL_STATE.emailError,
 });
 
-export const authPasswordReset = () => ({
+export const passwordReset = () => ({
   type: types.AUTH_PASSWORD_SETTER,
   payload: AUTH_INITIAL_STATE.password,
 });
 
-export const authPasswordErrorReset = () => ({
+export const passwordErrorReset = () => ({
   type: types.AUTH_PASSWORD_ERROR_SETTER,
   payload: AUTH_INITIAL_STATE.passwordError,
 });
 
-export const authLoginSignup = (payload) => ({
+export const loginSignup = (payload) => ({
   type: types.AUTH_LOGIN_SIGNUP,
   payload,
 });
 
-export const authLogout = () => ({
+export const logout = () => ({
   type: types.AUTH_LOGOUT,
 });
 
-export const authCheckIfLoggedIn = () => ({
+export const checkIfLoggedIn = () => ({
   type: types.CHECKIFLOGGEDIN,
 });
 
-export const authLoginSuccess = (payload) => ({
+export const loginSuccess = (payload) => ({
   type: types.AUTH_LOGIN_SUCCESS,
   payload,
 });
 
-export const authSignupSuccess = () => ({
+export const signupSuccess = () => ({
   type: types.AUTH_SIGNUP_SUCCESS,
 });
 
-export const authFailure = (payload) => ({
+export const failure = (payload) => ({
   type: types.AUTH_FAILURE,
   payload,
 });
